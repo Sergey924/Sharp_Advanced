a0b2b2edf578890f05e8ecd653ad288ea754026f
Sergey924 <74462379+Sergey924@users.noreply.github.com>
2020-12-26 17:12:38 +0200

    1

---
+++ HomeTask2/Extension method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask2
+{
+    static public class Extension_method
+    {
+        public static void GetArray(this MyList<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
